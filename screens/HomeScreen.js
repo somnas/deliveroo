b83cac32c6a406c6af5f56ead034073c548354c0
Erik Somnäs <erik.somnas@gmail.com>
2022-12-27 16:00:02 +0100

    Homescreen wip

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen() {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, []);
+
     return (
-        <View className='flex-1 items-center justify-center'>
-            <Text className='text-blue-900'>HomeScreen</Text>
-        </View>
+        <SafeAreaView>
+            {/* HEADER */}
+            <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+                <Image source={{
+                    uri: 'https://links.papareact.com/wru'
+                }}
+                    className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+                />
+                <View>
+                    <Text>Deliver Now!</Text>
+                    <Text>Current Location</Text>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
