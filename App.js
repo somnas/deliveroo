cd982f02ded12cce86dfaa1a1e4fe3f27f87015f
Erik Somnäs <erik.somnas@gmail.com>
2022-12-27 15:48:17 +0100

    create HomeScreen component

--- App.js
+++ App.js
@@ -3,15 +3,9 @@ import { Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
-	function HomeScreen() {
-		return (
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-				<Text className='text-red-500'>Home Screen</Text>
-			</View>
-		);
-	}
 
 	const Stack = createNativeStackNavigator();
 
