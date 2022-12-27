f5a257cbc45236e5a1365044eabdcb2ea880ff80
Erik Somnäs <erik.somnas@gmail.com>
2022-12-27 16:06:58 +0100

    homescreen header wip

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Image } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from "react-native-heroicons/outline";
 
 export default function HomeScreen() {
     const navigation = useNavigation();
@@ -21,9 +22,10 @@ export default function HomeScreen() {
                     className='h-7 w-7 bg-gray-300 p-4 rounded-full'
                 />
                 <View>
-                    <Text>Deliver Now!</Text>
-                    <Text>Current Location</Text>
+                    <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+                    <Text className='font-bold text-xl'>Current Location <ChevronDownIcon size={20} color='#00CCBB' /></Text>
                 </View>
+                <UserIcon size={35} color='#00CCBB' />
             </View>
         </SafeAreaView>
     )
